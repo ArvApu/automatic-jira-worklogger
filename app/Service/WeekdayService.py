a22ee0ac4300fb62e9445014de4682a8a43ca6f9
Arvydas Apulskis <arvydas.apulskis@twoday.com>
2024-02-06 11:01:23 +0200

    Move out weekdays related methods to a service

---
+++ app/Service/WeekdayService.py
@@ -0,0 +1,11 @@
+from datetime import datetime, timedelta
+from app.Enum.Weekday import Weekday
+
+def getStartOfTheWeek() -> datetime:
+    today = datetime.now()
+    startOfTheWeek = today - timedelta(days=today.weekday())
+
+    return startOfTheWeek.replace(hour=0, minute=0, second=0, microsecond=0)
+
+def getDayDateOfWeek(startOfTheWeekDate: datetime, weekday: Weekday) -> datetime:
+    return startOfTheWeekDate + timedelta(days=(weekday.value - 1))
--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import os
 
-from datetime import datetime, timedelta
 from dotenv import load_dotenv
 
 from app.Entity.JiraClientConfiguration import JiraClientConfiguration
-from app.Enum.Weekday import Weekday
 from app.Service import WorkdaysProvider
+from app.Service import WeekdayService
 from app.Client import JiraClient
 
 def main() -> None:
-    startOfTheWeek = getStartOfTheWeek()
+    startOfTheWeek = WeekdayService.getStartOfTheWeek()
     config = getJiraClientConfiguration()
     workerId = JiraClient.getWorkerId(config)
 
@@ -19,21 +18,12 @@ def main() -> None:
                 {
                     'originTaskId': issue.key,
                     'timeSpentSeconds': issue.timeSpentHours * 3600,
-                    'started': getDayDateOfWeek(startOfTheWeek, day.dayId).strftime('%Y-%m-%d'),
+                    'started': WeekdayService.getDayDateOfWeek(startOfTheWeek, day.dayId).strftime('%Y-%m-%d'),
                     'comment': issue.comment,
                     'worker': workerId,
                 },
                 config
             )
-
-def getStartOfTheWeek() -> datetime:
-    today = datetime.now()
-    startOfTheWeek = today - timedelta(days=today.weekday())
-
-    return startOfTheWeek.replace(hour=0, minute=0, second=0, microsecond=0)
-
-def getDayDateOfWeek(startOfTheWeekDate: datetime, weekday: Weekday) -> datetime:
-    return startOfTheWeekDate + timedelta(days=(weekday.value - 1))
 
 def getJiraClientConfiguration() -> JiraClientConfiguration:
     load_dotenv('.env')
